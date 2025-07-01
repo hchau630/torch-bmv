280d8b49e98a31e1ef40dd5476133d50684d3029
Ho Yin Chau <hchau630@berkeley.edu>
2025-07-01 17:40:23 -0400

    fix asv config and benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -3,10 +3,10 @@ import torch
 from torch_bmv import bmv
 
 
-class BmvForward:
+class TimeBmvForward:
     params = (
         [500, 5000],
-        [((1,), (10,)), ((10,), (1,)), ((1, 1), (2, 5))],
+        [((1,), (10,)), ((10,), (1,)), ((1, 1), (1, 10)), ((1, 1), (2, 5))],
         [False, True],
     )
     param_names = ["n", "bshapes", "naive"]
@@ -18,14 +18,27 @@ class BmvForward:
     def time_bmv_forward(self, n, bshapes, naive):
         bmv(self.A, self.x, naive=naive)
 
+
+class MemBmvForward:
+    params = (
+        [500, 5000],
+        [((1,), (10,)), ((10,), (1,)), ((1, 1), (1, 10)), ((1, 1), (2, 5))],
+        [False, True],
+    )
+    param_names = ["n", "bshapes", "naive"]
+
+    def setup(self, n, bshapes, naive):
+        self.A = torch.randn(*bshapes[0], n, n)
+        self.x = torch.randn(*bshapes[1], n)
+
     def peakmem_bmv_forward(self, n, bshapes, naive):
         bmv(self.A, self.x, naive=naive)
 
 
-class BmvBackward:
+class TimeBmvBackward:
     params = (
         [500, 5000],
-        [((1,), (10,)), ((10,), (1,)), ((1, 1), (2, 5))],
+        [((1,), (10,)), ((10,), (1,)), ((1, 1), (1, 10)), ((1, 1), (2, 5))],
         [False, True],
     )
     param_names = ["n", "bshapes", "naive"]
@@ -37,6 +50,20 @@ class BmvBackward:
 
     def time_bmv_backward(self, n, bshapes, naive):
         self.out.backward()
+
+
+class MemBmvBackward:
+    params = (
+        [500, 5000],
+        [((1,), (10,)), ((10,), (1,)), ((1, 1), (1, 10)), ((1, 1), (2, 5))],
+        [False, True],
+    )
+    param_names = ["n", "bshapes", "naive"]
+
+    def setup(self, n, bshapes, naive):
+        A = torch.randn(*bshapes[0], n, n, requires_grad=True)
+        x = torch.randn(*bshapes[1], n)
+        self.out = bmv(A, x, naive=naive).mean()
 
     def peakmem_bmv_backward(self, n, bshapes, naive):
         self.out.backward()
