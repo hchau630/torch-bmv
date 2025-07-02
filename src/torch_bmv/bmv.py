116ca4bf804fc916e4eca5d64d6f3e1ce6559fb7
Ho Yin Chau <hchau630@berkeley.edu>
2025-07-02 19:53:16 -0400

    turns out einsum is just as efficient, this package is kinda stupid now

--- src/torch_bmv/bmv.py
+++ src/torch_bmv/bmv.py
@@ -14,35 +14,8 @@ def bmv(A: Tensor, x: Tensor, naive: bool = False) -> Tensor:
     Returns:
         Tensor: Tensor with shape (*, m).
     """
-    if A.ndim < 2 or x.ndim < 1:
-        raise ValueError(
-            "A must have at least 2 dimensions and x must have at least 1 dimension."
-        )
-
     if naive:
-        # Don't optimize, just do the multiplication.
-        # return (A @ x.unsqueeze(-1)).squeeze(-1)
-        return torch.einsum("...ij,...j->...i", A, x)
-
-    nbatchdim = max(A.ndim - 2, x.ndim - 1)
-    A = A[(None,) * (nbatchdim - A.ndim + 2)]
-    x = x[(None,) * (nbatchdim - x.ndim + 1)]
-    bshape_A = A.shape[:-2]
-    bshape_x = x.shape[:-1]
-    assert len(bshape_A) == len(bshape_x), "Batch dimensions must match."
-
-    dims = [d for d, (i, j) in enumerate(zip(bshape_A, bshape_x)) if i == 1 and j > 1]
-    if len(dims) == 0:
-        # No optimization possible, just do the multiplication.
         return (A @ x.unsqueeze(-1)).squeeze(-1)
 
-    new_dims = tuple(range(-len(dims), 0))
-    A = A.squeeze(dims)
-    x = x.movedim(dims, new_dims)
-    shape = x.shape[-len(dims) :]
-    x = x.reshape(*x.shape[: -len(dims)], -1)
-    out = A @ x
-    out = out.reshape(*out.shape[:-1], *shape)
-    out = out.movedim(new_dims, dims)
-
-    return out
+    # einsum is much more efficient than matmul, see pytorch github issue #110858
+    return torch.einsum("...ij,...j->...i", A, x)
